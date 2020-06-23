c208dbee6c744610b253df7e23375c9ea938a4aa
Ubuntu <ubuntu@ip-172-31-30-135.us-east-2.compute.internal>
2020-06-23 20:08:23 +0000

    Add setup file for python packages

---
+++ setup.sh
@@ -0,0 +1,25 @@
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py
+export PATH="$HOME/.local/bin:$PATH"
+
+pip install -U spacy
+
+pip install -U scikit-learn
+
+pip install rake-nltk
+
+pip install scispacy
+
+pip install pytextrank
+
+python -m pip install -U matplotlib
+
+pip install pandas
+
+pip install nltk
+
+pip install pyLDAvis
+
+pip install rank-bm25
+
+pip install Keras
