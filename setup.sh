7216745abf9c38cf84f753e94a2ce005586bf0ae
Rishi Patel <rpatel@cern.ch>
2020-07-02 20:54:35 +0200

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,7 @@
+sudo apt-get update
+
+sudo yum install python3
+
 curl -O https://bootstrap.pypa.io/get-pip.py
 python3 get-pip.py
 export PATH="$HOME/.local/bin:$PATH"
@@ -10,7 +14,14 @@ pip install -U scikit-learn
 
 pip install rake-nltk
 
-pip install scispacy
+curl -O https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-x86_64.sh
+
+sha256sum Anaconda3-2020.02-Linux-x86_64.sh
+
+bash Anaconda3-2020.02-Linux-x86_64.sh
+
+conda install numpy
+conda install -c akode nmslib
 
 pip install pytextrank
 
@@ -25,3 +36,8 @@ pip install pyLDAvis
 pip install rank-bm25
 
 pip install Keras
+
+pip install scispacy
+
+sudo apt-get install git
+
