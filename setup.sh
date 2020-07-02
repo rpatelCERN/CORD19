36f58517fe9caa750333aeb50308fb2fb02804f3
Rishi Patel <rpatel@cern.ch>
2020-07-02 22:38:04 +0200

    Update setup

--- setup.sh
+++ setup.sh
@@ -8,7 +8,8 @@ export PATH="$HOME/.local/bin:$PATH"
 
 pip install gensim
 
-pip install -U spacy
+pip install spacy==2.2.4
+python3 -m spacy download en_core_web_sm
 
 pip install -U scikit-learn
 
@@ -27,9 +28,9 @@ pip install pyLDAvis
 
 pip install rank-bm25
 
-pip install Keras
 
 pip install scispacy --no-deps
 
-sudo yum install git
+pip install https://s3-us-west-2.amazonaws.com/ai2-s2-scispacy/releases/v0.2.4/en_core_sci_sm-0.2.4.tar.gz
 
+sudo yum install git
